8f877749cb7a8e96f99af139241b5dcefee57a80
Fernando Bernal <statefull@gmail.com>
2019-01-22 20:18:26 +0100

    deep clone

---
+++ deepclone.js
@@ -0,0 +1,22 @@
+Object.prototype.clone = function () {
+  let cloned = null;
+
+  if (Array.isArray(this)) {
+
+    cloned = [];
+    this.forEach((e) => {
+      cloned.push(typeof e === 'object' ? e.clone() : e);
+    });
+
+  } else if (typeof this === 'object') {
+    cloned = {};
+
+    Object.keys(this).forEach((key) => {
+      cloned[key] = typeof this[key] === 'object' ? this[key].clone() : this[key];
+    });
+  } else {
+    return this;
+  }
+
+  return cloned;
+}
